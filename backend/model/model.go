6ac036f8df1c26bacac2cd40f0c8acdd55f884f8
usernob <shamsulmaarif2016@gmail.com>
2024-02-01 10:47:43 +0700

    fix: docker compose for development

--- backend/model/model.go
+++ backend/model/model.go
@@ -1,6 +1,10 @@
 package model
 
 import (
+	"backend/pkg/passwordhash"
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -9,7 +13,13 @@ var db *gorm.DB
 
 func init() {
 	var err error
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -19,6 +29,19 @@ func init() {
 
 func Migrate() {
 	db.AutoMigrate(&User{}, &Admin{}, &Film{}, &Genre{}, &Audiotorium{}, &Penayangan{}, &Tiket{}, &Kursi{}, &Seat{})
+
+  hashedPassword, err := passwordhash.HashPassword(os.Getenv("SUPERADMIN_PASSWORD"))
+
+  if err != nil {
+    panic(err)
+  }
+
+  db.Create(&Admin{
+    Nama: os.Getenv("SUPERADMIN_NAME"),
+    Email: os.Getenv("SUPERADMIN_EMAIL"),
+    Password: hashedPassword,
+    Role: "superadmin",
+  })
 }
 
 func SeedRefresh() {
